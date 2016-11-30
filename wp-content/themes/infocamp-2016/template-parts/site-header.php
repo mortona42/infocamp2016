7333cb6ae3511723a32b6656a0024408e5634816
Andrew Morton <mortona42@gmail.com>
2016-11-29 22:23:02 -0800

    Tweak responsive styles

--- wp-content/themes/infocamp-2016/template-parts/site-header.php
+++ wp-content/themes/infocamp-2016/template-parts/site-header.php
@@ -51,9 +51,9 @@
         </div>
     </div>
 
-    <nav id="site-navigation" class="main-navigation" role="navigation">
-        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'infocamp-2016' ); ?></button>
-        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-    </nav><!-- #site-navigation -->
-
 </header><!-- #masthead -->
+
+<nav id="site-navigation" class="main-navigation" role="navigation">
+    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'infocamp-2016' ); ?></button>
+    <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+</nav><!-- #site-navigation -->
