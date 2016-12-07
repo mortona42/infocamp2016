8b1a0320fbbd0c2eb46116e5a2e663af0adefd05
Andrew Morton <mortona42@gmail.com>
2016-12-06 21:19:56 -0800

    Switch to full width banner img

--- wp-content/themes/infocamp-2016/template-parts/site-header.php
+++ wp-content/themes/infocamp-2016/template-parts/site-header.php
@@ -7,52 +7,56 @@
 <?php get_template_part( 'template-parts/site-menu' ); ?>
 
 <header id="masthead" class="site-header" role="banner">
-
     <?php if ( get_header_image() ) : ?>
         <img class='hero-img' src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
     <?php endif; // End header image check. ?>
 
-    <div class="site-header_overlay_wrapper">
-        <div class="site-header_overlay_content">
+    <div id="header-wrapper">
+
+        <div class="site-header_overlay_wrapper">
 
 
-<!--                --><?php
-//                $description = get_bloginfo( 'description', 'display' );
-//                if ( $description || is_customize_preview() ) : ?>
-<!--                    <p id="site-description" class="site-description">--><?php //echo $description; /* WPCS: xss ok. */ ?><!--</p>-->
-<!--                    --><?php
-//                endif; ?>
+            <div class="site-header_overlay_content">
 
 
-            <?php
-            if ( ! $event_location = get_theme_mod( 'event_description' )) {
-                $event_location = 'Configure description with the customizer.';
-            }
-            ?>
-            <div id="event-description"><?php echo $event_location; ?></div>
+    <!--                --><?php
+    //                $description = get_bloginfo( 'description', 'display' );
+    //                if ( $description || is_customize_preview() ) : ?>
+    <!--                    <p id="site-description" class="site-description">--><?php //echo $description; /* WPCS: xss ok. */ ?><!--</p>-->
+    <!--                    --><?php
+    //                endif; ?>
 
 
-            <?php
-            if ( get_theme_mod( 'event_date' )) {
-                $event_date = get_theme_mod( 'event_date');
+                <?php
+                if ( ! $event_location = get_theme_mod( 'event_description' )) {
+                    $event_location = 'Configure description with the customizer.';
+                }
+                ?>
+                <div id="event-description"><?php echo $event_location; ?></div>
 
-                // Convert date format.
-                $event_date = date_create_from_format('Y-m-d', $event_date);
-                $event_date = $event_date->format('F j, Y');
-            }
-            else {
-                $event_date = 'Configure date with the customizer.';
-            }
-            ?>
-            <p id="event-date"><?php echo $event_date; ?></p>
 
-            <?php
-            if ( ! $event_location = get_theme_mod( 'event_location' )) {
-                $event_location = 'Configure location with the customizer.';
-            }
-            ?>
-            <p id="event-location"><?php echo $event_location; ?></p>
+                <?php
+                if ( get_theme_mod( 'event_date' )) {
+                    $event_date = get_theme_mod( 'event_date');
 
+                    // Convert date format.
+                    $event_date = date_create_from_format('Y-m-d', $event_date);
+                    $event_date = $event_date->format('F j, Y');
+                }
+                else {
+                    $event_date = 'Configure date with the customizer.';
+                }
+                ?>
+                <p id="event-date"><?php echo $event_date; ?></p>
+
+                <?php
+                if ( ! $event_location = get_theme_mod( 'event_location' )) {
+                    $event_location = 'Configure location with the customizer.';
+                }
+                ?>
+                <p id="event-location"><?php echo $event_location; ?></p>
+
+            </div>
         </div>
     </div>
 
