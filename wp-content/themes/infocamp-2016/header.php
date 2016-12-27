924c8bf37de2020ae1d57a9af44a51930ed6aa5d
klpittman <klpittman@gmail.com>
2016-12-26 16:50:30 -0800

    adding font-awesome icons and menu button icon

--- wp-content/themes/infocamp-2016/header.php
+++ wp-content/themes/infocamp-2016/header.php
@@ -16,7 +16,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
+<script src="https://use.fontawesome.com/1ec6b8b1a8.js"></script>
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i|Roboto+Slab:400,700" rel="stylesheet">
+
 
 <?php wp_head(); ?>
 </head>
