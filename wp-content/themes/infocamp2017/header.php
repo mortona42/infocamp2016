2fa503a94b9ed03f074a07132aa4643095d38051
Andrew Morton <mortona42@gmail.com>
2017-01-09 23:22:07 -0800

    Add wrapper to navigation

--- wp-content/themes/infocamp2017/header.php
+++ wp-content/themes/infocamp2017/header.php
@@ -23,24 +23,28 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'infocamp2017' ); ?></a>
 
-    <nav id="site-navigation" class="main-navigation" role="navigation">
-        <div class="site-branding">
-            <?php
-            if ( is_front_page() && is_home() ) : ?>
-                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-            <?php else : ?>
-                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+    <div id="navigation-wrapper">
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+            <div class="site-branding">
                 <?php
-            endif; ?>
-        </div><!-- .site-branding -->
-        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'infocamp2017' ); ?></button>
-        <?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-    </nav><!-- #site-navigation -->
+                if ( is_front_page() && is_home() ) : ?>
+                    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <?php else : ?>
+                    <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                    <?php
+                endif; ?>
+            </div><!-- .site-branding -->
+            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'infocamp2017' ); ?></button>
+            <?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+        </nav><!-- #site-navigation -->
+    </div>
 
     <?php
     $description = get_bloginfo( 'description', 'display' );
     if ( $description || is_customize_preview() ) : ?>
-        <span class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></span>
+        <div class="site-description">
+            <span><?php echo $description; /* WPCS: xss ok. */ ?></span>
+        </div>
         <?php
     endif; ?>
 	</header><!-- #masthead -->
