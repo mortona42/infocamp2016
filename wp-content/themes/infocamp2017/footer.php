860fac1e11a1473c3c9fc372f125ddd7de3e811b
Andrew Morton <mortona42@gmail.com>
2017-01-09 21:41:35 -0800

    Remove footer content

--- wp-content/themes/infocamp2017/footer.php
+++ wp-content/themes/infocamp2017/footer.php
@@ -15,9 +15,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'infocamp2017' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'infocamp2017' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'infocamp2017' ), 'infocamp2017', '<a href="https://automattic.com/" rel="designer">Andrew Morton</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
