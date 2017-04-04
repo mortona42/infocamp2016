53f7b191beb13c493e76e12235f9315b2f42b754
klpittman <klpittman@gmail.com>
2017-04-04 07:37:44 -0400

    adding sponsor logos to home page

--- wp-content/themes/infocamp2017/front-page.php
+++ wp-content/themes/infocamp2017/front-page.php
@@ -32,42 +32,68 @@ get_header(); ?>
                 $about_header = get_theme_mod( 'about_header' );
                 $about_copy = get_theme_mod( 'about_copy' );
                 ?>
-                <section id="event-about">
+
+                <div class="event-wrapper">
+                    <section id="event-about">
+                        <div class="front-page-section_wrapper">
+                            <h2><?php echo $about_header; ?></h2>
+                            <p><?php echo $about_copy; ?></p>
+                        </div>
+                    </section>
+
+                    <?php
+                    $participate_header = get_theme_mod( 'participate_header' );
+                    $participate_copy = get_theme_mod( 'participate_copy' );
+
+                    $twitter_url = get_theme_mod( 'twitter_url' );
+                    $twitter_button = get_theme_mod( 'twitter_button' );
+                    $facebook_url = get_theme_mod( 'facebook_url' );
+                    $facebook_button = get_theme_mod( 'facebook_button' );
+                    ?>
+                    <section id="event-participate">
+                        <div class="front-page-section_wrapper">
+                            <h2><?php echo $participate_header; ?></h2>
+                            <p><?php echo $participate_copy; ?></p>
+
+                            <div id="button-wrapper">
+
+                                <div class="button-link">
+                                <a  href="<?php echo $twitter_url; ?>">
+                                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                                    <span><?php echo $twitter_button; ?></span>
+                                </a>
+                                </div>
+
+                                <div class="button-link">
+                                <a  href="<?php echo $facebook_url; ?>">
+                                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                                    <span><?php echo $facebook_button; ?></span>
+                                </a>
+                                </div>
+                        </div>
+                    </section>
+                </div>
+
+                <section id="event-sponsors">
                     <div class="front-page-section_wrapper">
-                        <h2><?php echo $about_header; ?></h2>
-                        <p><?php echo $about_copy; ?></p>
-                    </div>
-                </section>
+                        <h2>Sponsors</h2>
+                        <p>Thank you to our generous sponsors for helping make InfoCamp possible.</p>
 
-                <?php
-                $participate_header = get_theme_mod( 'participate_header' );
-                $participate_copy = get_theme_mod( 'participate_copy' );
-
-                $twitter_url = get_theme_mod( 'twitter_url' );
-                $twitter_button = get_theme_mod( 'twitter_button' );
-                $facebook_url = get_theme_mod( 'facebook_url' );
-                $facebook_button = get_theme_mod( 'facebook_button' );
-                ?>
-                <section id="event-participate">
-                    <div class="front-page-section_wrapper">
-                        <h2><?php echo $participate_header; ?></h2>
-                        <p><?php echo $participate_copy; ?></p>
-
-                        <div id="button-wrapper">
-
-                            <div class="button-link">
-                            <a  href="<?php echo $twitter_url; ?>">
-                                <i class="fa fa-twitter" aria-hidden="true"></i>
-                                <span><?php echo $twitter_button; ?></span>
+                        <div class="sponsor-row">
+                            <a href="https://www.asist.org">
+                                <img src="/wp-content/themes/infocamp2017/images/ASIST-PNW-logo.jpg" alt="Association for Information Science and Technology" />
                             </a>
-                            </div>
+                        </div>
 
-                            <div class="button-link">
-                            <a  href="<?php echo $facebook_url; ?>">
-                                <i class="fa fa-facebook" aria-hidden="true"></i>
-                                <span><?php echo $facebook_button; ?></span>
+                        <div class="sponsor-row inkind">
+                            <a href="https://www.axure.com">
+                                <img src="/wp-content/themes/infocamp2017/images/axurelogo-400.png" alt="Axure" />
                             </a>
-                            </div>
+
+                            <a href="http://rosenfeldmedia.com">
+                                <img src="/wp-content/themes/infocamp2017/images/RM-Logo_logotype.gif" alt="Rosenfeld" />
+                            </a>
+                        </div>
                     </div>
                 </section>
 
